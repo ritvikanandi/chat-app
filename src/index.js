ece81ad85490c9bad3cc157385b536d94cafc5d2
ritvika <ritvika30@gmail.com>
2021-06-15 19:14:29 +0530

    Socket.io added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+//requiring socket.io library
+const socketio = require("socket.io");
 const app = express();
+//create a new http server
+const server = http.createServer(app);
+//socket.io uses a raw server which we cannot access through express
+const io = socketio(server);
 const port = process.env.PORT || 3000;
 const publicdirectorypath = path.join(__dirname, "../public");
 
 app.use(express.static(publicdirectorypath));
 
-app.listen(port, () => {
+//firing events
+io.on("connection", () => {
+  console.log("New Web Socket connection");
+});
+
+server.listen(port, () => {
   console.log("Listening to server");
 });
