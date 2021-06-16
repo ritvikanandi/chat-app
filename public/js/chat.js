847bc023ddad5ddf933da767f61b20d761d7e0d5
ritvika <ritvika30@gmail.com>
2021-06-16 23:31:19 +0530

    bad-words,broadcast emit added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,52 @@
-io();
+const socket = io();
+//Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#location-send");
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  //disable
+  $messageFormButton.setAttribute("disabled", "disabled");
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    //enable
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The message was sent!");
+  });
+});
+
+document.querySelector("#location-send").addEventListener("click", () => {
+  //disable
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported in your browser");
+  }
+  $sendLocationButton.setAttribute("disabled", "disabled");
+  //location event
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    //enable
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location shared excellent!");
+        $sendLocationButton.removeAttribute("disabled");
+      }
+    );
+  });
+});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 //requiring socket.io library
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 const app = express();
 //create a new http server
 const server = http.createServer(app);
@@ -14,8 +15,30 @@ const publicdirectorypath = path.join(__dirname, "../public");
 app.use(express.static(publicdirectorypath));
 
 //firing events
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New Web Socket connection");
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed");
+    }
+    io.emit("message", message);
+    callback();
+  });
+  //in built event
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
+  });
+  //geolocation event
+  socket.on("sendLocation", (obj, callback) => {
+    socket.broadcast.emit(
+      "message",
+      `https://google.com/maps?q=${obj.latitude},${obj.longitude}`
+    );
+    callback();
+  });
 });
 
 server.listen(port, () => {
